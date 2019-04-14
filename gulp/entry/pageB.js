873eb3f23b9f0b85597b8d943db1f4f0b8ca0e9b
HelloAndo <331754520@qq.com>
2019-04-14 21:42:23 +0800

    gulp

---
+++ gulp/entry/pageB.js
@@ -0,0 +1,16 @@
+import $ from 'jquery'
+
+import common from '../js/common'
+
+class Server {
+  constructor () {
+
+    this.init()
+  }
+
+  init () {
+    console.log('server')
+  }
+}
+
+export default new Server()
